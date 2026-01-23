73f84fe576471488759c1cc8e43b6096e6d53b70
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-23 11:44:26 +0530

    Added Constructor Demo

---
+++ 02-OOPS-Concepts/1Constructors/1Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿class Employee
+{
+    public int Id { get; }
+    public string Name { get; }
+
+
+    // Constructor
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Employee emp = new Employee(1, "Seenu");
+        Console.WriteLine(emp.Name);
+    }
+}
