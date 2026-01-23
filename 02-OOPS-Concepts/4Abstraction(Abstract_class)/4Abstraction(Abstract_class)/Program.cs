d4e343551547750afe714f3afc55753845ac7ca9
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-23 12:02:28 +0530

    Added Abstract Demo

---
+++ 02-OOPS-Concepts/4Abstraction(Abstract_class)/4Abstraction(Abstract_class)/Program.cs
@@ -0,0 +1,23 @@
+﻿abstract class Payment              //Abstract class cannot be instantiated
+{
+    public abstract void Pay();     //Can have abstract + non-abstract methods
+}
+
+
+class CreditCardPayment : Payment
+{
+    public override void Pay()
+    {
+        Console.WriteLine("Payment done using credit card");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Payment payment = new CreditCardPayment();
+        payment.Pay();
+    }
+}
