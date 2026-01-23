1974c0d42f73f8b36c57dd8ea6dd829c6d79b144
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-23 11:58:21 +0530

    Added Polymorphism Demo

---
+++ 02-OOPS-Concepts/3Polymorphism(overriding)/3Polymorphism(overriding)/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Notes:
+Parent method must be virtual
+Child method must be override
+*/
+class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal makes sound");
+    }
+}
+
+
+class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal a = new Dog();
+        a.Sound(); // Dog barks
+    }
+}
