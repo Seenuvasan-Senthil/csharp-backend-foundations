b96baf9a91bffa8e8e794b7d1d7a508a1104c11c
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-23 12:04:40 +0530

    Added Interface Demo

---
+++ 02-OOPS-Concepts/5Interfaces/5Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿interface ILogger
+{
+    void Log(string message);      //Interface methods are public by default, No implementation inside interface.
+}
+
+
+class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("File log: " + message);
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        ILogger logger = new FileLogger();
+        logger.Log("Application started");
+    }
+}
