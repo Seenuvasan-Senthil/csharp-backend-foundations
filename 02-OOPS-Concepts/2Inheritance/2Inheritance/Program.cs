6fed7f7f8a90059afe18b8d739552b30d02d9b14
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-23 11:50:51 +0530

    Added Inheritance Demo

---
+++ 02-OOPS-Concepts/2Inheritance/2Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿class Person
+{
+    public string Name { get; set; }
+
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello, I am " + Name);
+    }
+}
+
+
+class Employee : Person
+{
+    public int EmployeeId { get; set; }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Employee emp = new Employee();
+        emp.Name = "Seenu";
+        emp.EmployeeId = 101;
+        emp.Introduce();
+    }
+}
