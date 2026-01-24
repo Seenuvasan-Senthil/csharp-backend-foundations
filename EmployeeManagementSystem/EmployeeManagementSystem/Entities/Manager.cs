39c41272c426c2879cdbb0dd54bcad28cb53991d
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-24 18:46:11 +0530

    feat(entities): introduce manager entity

--- EmployeeManagementSystem/EmployeeManagementSystem/Entities/Manager.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Entities/Manager.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace EmployeeManagementSystem.Entities
 {
-    internal class Manager
+    public class Manager : Employee
     {
+        //manager's specific fields are here
+        private int _teamSize; //uncontrolled access prevention
+        
+        //controlled access via property below
+        public int TeamSize
+        {
+            get { return _teamSize; }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("TeamSize Cannot be Empty .");
+                _teamSize = value;
+            }
+        }
+
+        //paramettarized constructor - creates valid object
+        public Manager(int id, string name, string department, decimal salary, int teamSize) : base(id, name, department, salary)
+        {
+            TeamSize = teamSize;
+        }
+
+        //polymorphism - manager specific details - overriding generic emp method
+        public override string GetEmployeeDetails()
+        {
+            return $"{base.GetEmployeeDetails()}, TeamSize: {TeamSize}"; //"base"->like "super" keyword in java
+        }
     }
 }
