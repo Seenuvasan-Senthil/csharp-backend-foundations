a79d35b527bea353c2c01ae403dfbb373c028c6f
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-27 19:38:59 +0530

    feat(menus): role based menu options introduced

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Menus/EmployeeMenu.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagementSystem.Entities;
+using EmployeeManagementSystem.Services;
+
+namespace EmployeeManagementSystem.Menus
+{
+    internal class EmployeeMenu
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly RequestService _requestService;
+        private readonly Role _role;
+
+        public EmployeeMenu(IEmployeeService employeeService, RequestService requestService, Role role)
+        {
+            _employeeService = employeeService;
+            _requestService = requestService;
+            _role = role;
+        }
+
+        public void Show()
+        {
+            Employee employee = AuthenticateEmployee();
+
+            if(employee == null)
+            {
+                Console.WriteLine("Invalid emp Id or Role Mismatch");
+                return;
+            }
+
+            bool logout = false;
+
+            while (!logout)
+            {
+                Console.WriteLine($"\n---{_role} Menu---");
+                Console.WriteLine("1. View Profile.");
+                Console.WriteLine("2. Raise Request");
+                Console.WriteLine("0. Logout");
+                Console.WriteLine("Choose Option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ShowProfile(employee);
+                        break;
+
+                    case "2":
+                        RaiseRequest(employee);
+                        break;
+
+                    case "0":
+                        logout = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Operation.");
+                        break;
+                }
+            }
+        }
+
+        private Employee AuthenticateEmployee()
+        {
+            Console.WriteLine("Enter Employee ID: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int id))
+                return null;
+
+            Employee employee = _employeeService.GetEmployeeById(id);
+
+            if (employee == null) return null;
+
+            //role vadation part
+            if (_role == Role.Manager && employee is not Manager) return null;
+
+            if (_role == _role.Tester && employee is not Tester) return null;
+
+            return employee;
+        }
+
+        private void ShowProfile(Employee employee)
+        {
+            Console.WriteLine("\n---My Profile---");
+            Console.WriteLine($"ID: {employee.Id}");
+            Console.WriteLine($"Name: {employee.Name}");
+            Console.WriteLine($"Department: {employee.Department}");
+            Console.WriteLine($"Salary: {employee.Salary}");
+        }
+
+        private void RaiseRequest(Employee employee)
+        {
+            Console.WriteLine("Enter Request Message: ");
+            string message = Console.ReadLine();
+            _requestService.AddRequest(employee.Id, message);
+            Console.WriteLine("Request Raised Successfully.");
+        }
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Menus/HRMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using EmployeeManagementSystem.Services;
+using System;
+using System.Net.Security;
+
+namespace EmployeeManagementSystem.Menus
+{
+    internal class HRMenu
+    {
+        private const int HR_SECRET_KEY = 10;
+        private readonly IEmployeeService _employeeService;
+
+        public HRMenu(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public void Show()
+        {
+            if (!Authenticate())
+            {
+                Console.WriteLine("Invalid Hr Key. So, Access deied.");
+                return;
+            }
+
+            bool logout = false;
+
+            while (!logout)
+            {
+                Console.WriteLine("\n---HR Menu---");
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Remove Employee");
+                Console.WriteLine("3. View All Employee");
+                Console.WriteLine("4. Update Salary");
+                Console.WriteLine("0. Logout");
+                Console.WriteLine("Choose Option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        //Add
+                        break;
+
+                    case "2":
+                        //Remove
+                        break;
+
+                    case "3":
+                        //ViewAll
+                        break;
+
+                    case "4":
+                        //UpdateSalary
+                        break;
+
+                    case "0":
+                        logout = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option .");
+                        break;
+                }
+            }
+        }
+
+        private bool Authenticate()
+        {
+            Console.WriteLine("Enter Hr Secret Key: ");
+            return int.TryParse(Console.ReadLine(), out int key) && key == HR_SECRET_KEY; 
+        }
+    }
+}
