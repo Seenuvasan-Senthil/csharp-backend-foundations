7e73e845e428e9a3a33c54020e5aa54eac6341d9
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-27 22:38:27 +0530

    feat(entities): introduce request entity

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Entities/Request.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagementSystem.Enums;
+
+namespace EmployeeManagementSystem.Entities
+{
+    internal class Request
+    {
+        public int Id { get; private set; }
+        public int EmployeeId { get; }
+        public Role RequestedByRole { get; }
+        public string Description { get; }
+        public string Status { get; private set; }
+
+        public Request(int empId, Role role, string description)
+        {
+            Id = empId;
+            EmployeeId = empId;
+            Description = description;
+            Status = "Pending";
+        }
+
+        internal void SetId(int id) {
+            if (Id != 0) throw new InvalidOperationException("Request Id Already Set.");
+            Id = id;
+        }
+
+        internal void Approve()
+        {
+            Status = "Approved";
+        }
+
+        internal void Rejected()
+        {
+            Status = "Rejected";
+        }
+    }
+}
