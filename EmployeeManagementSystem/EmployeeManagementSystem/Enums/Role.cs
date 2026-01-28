e00cc0847805710271b76efe69945a2ab445e07f
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-28 23:28:11 +0530

    feat(entities): introduce developer entity

--- EmployeeManagementSystem/EmployeeManagementSystem/Enums/Role.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Enums/Role.cs
@@ -4,5 +4,5 @@ using System.Text;
 
 namespace EmployeeManagementSystem.Enums
 {
-    public enum Role { Manager, Tester}//enums are types, types must be shared, not duplicated.
+    public enum Role { Manager, Tester, Developer}//enums are types, types must be shared, not duplicated.
 }
