38c6cfff945245235bdcdf210942d3ace628ffba
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-01-28 12:32:22 +0530

    docs(ems): added some comments

--- EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
@@ -11,7 +11,7 @@ namespace EmployeeManagementSystem
         {
             IEmployeeService employeeService = new EmployeeService();
             RequestService requestService = new RequestService();
-
+            //constructor dependency injection
             MainMenu mainMenu = new MainMenu(employeeService, requestService);
             mainMenu.show();
 
