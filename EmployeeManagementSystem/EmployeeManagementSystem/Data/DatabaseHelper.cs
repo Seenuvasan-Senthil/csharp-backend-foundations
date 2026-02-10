8df285f11d2e2b16ac39356d764b86c7aac2dbe0
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-02-10 23:03:00 +0530

    feat(data): database helper created for connection

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Data/DatabaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
+namespace EmployeeManagementSystem.Data
+{
+    public static class DatabaseHelper
+    {
+        private static string ConnectionString =
+            @"Server=localhost\SQLEXPRESS, Database=EmployeeManagementSystemDB; Trusted_Connection=true; Trust_Server_Certificate=true;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+
+        }
+    }
+}
