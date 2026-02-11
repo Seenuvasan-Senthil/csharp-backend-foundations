b91458b63a0437be0624206bd5d88f4672add23c
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-02-12 00:48:12 +0530

    feat(factories): RoleFactory introduced .

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Factories/RoleFactory.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagementSystem.Roles;
+
+namespace EmployeeManagementSystem.Factories
+{
+    internal static class RoleFactory
+    {
+        public static IRole Create(string roleName)
+        {
+            return roleName switch
+            {
+                "Manager" => new ManagerRole(),
+                "Developer" => new DeveloperRole(),
+                "Tester" => new TesterRole(),
+                _ => throw new Exception("Invalid role")
+            };
+        }
+    }
+}
