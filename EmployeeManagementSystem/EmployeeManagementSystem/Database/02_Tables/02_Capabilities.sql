b7e3f54948417f3778f8d4b978e71039b0f56f28
Seenuvasan-Senthil <ss931@apac.comcast.com>
2026-02-16 20:30:43 +0530

    fix(capabilities): capabilities inserts added

--- EmployeeManagementSystem/EmployeeManagementSystem/Database/02_Tables/02_Capabilities.sql
+++ EmployeeManagementSystem/EmployeeManagementSystem/Database/02_Tables/02_Capabilities.sql
@@ -5,3 +5,8 @@ CREATE TABLE Capabilities (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL
 );
+
+INSERT INTO Capabilities(Name) VALUES ('IApprover');
+INSERT INTO Capabilities(Name) VALUES ('ITeamSupervisor');
+INSERT INTO Capabilities(Name) VALUES ('ICodeContributer');
+INSERT INTO Capabilities(Name) VALUES ('ITestExecuter');
